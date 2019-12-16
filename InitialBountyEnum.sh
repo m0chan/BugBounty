c48625dc7834f557f2cae0890ad75700ca34173d
m0chan <aidan@aidesigns.net>
2019-12-16 15:12:14 +0000

    MassDNSScript

--- InitialBountyEnum.sh
+++ InitialBountyEnum.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-echo "Syntax, Arg1 = Domain, Arg2 = Output File"
-echo "Enumerating " $1
-echo "Consider using amass intel -org to find ASN and bruteforce/rev dns lookup CIDR range etc"
-subfinder -d $1 -o $2.initialdomains
-amass enum -d $1 | tee $2.initialdomains 
-python3 $BugBounty/CrtSH.py $1 | tee $2.initialdomains
-cat $2.initialdomains | grep -v "Average" | grep -v "Querying" | awk '!seen[$0]++' $2.initialdomains > $2
-echo "Found The Following Amount of SubDomains (Will Check for SubTakeover & End" && wc -l $2
+echo "\nSyntax, Arg1 = Domain, Arg2 = Output File"
+echo "\nEnumerating " $1
+echo "\nConsider using amass intel -org to find ASN and bruteforce/rev dns lookup CIDR range etc"
 
-echo "Starting Stage 2 - Service Discovery"
+subfinder -d $1 -o $2.initialdomains
+amass enum -d $1 | tee >> $2.initialdomains 
+python3 $BugBounty/CrtSH.py $1 | tee >> $2.initialdomains
+
+echo "\n\n Using ALTDNS to Find Permutations"
+
+altdns $2.initialdomains -o $2.initialdomains.permutations -w $Tools/altdns/words.txt -r -s $2.initialdomains.permutations.resolved
+cat $2.initialdomains.permutations.resolved >> $2.initialdomains
+cat $2.initialdomains | grep -v "Average" | grep -v "Querying" | awk '!seen[$0]++' $2.initialdomains > $2
+
+echo "\nFound The Following Amount of SubDomains (Will Check for SubTakeover & End" && wc -l $2
+
+echo "\nUse github-search tools in $Tools Manually Here" 
+
+echo "\nStarting Stage 2 - Service Discovery"
 cat $2 | httprobe -c 50 | tee $2.httprobe
-echo "Copying HTTP Servers to EyeWitness Dir" && cp $2.httprobe $Tools/EyeWitness
-echo "Starting DirSearch for Basic Paths on Found Hosts"
+echo "\nCopying HTTP Servers to EyeWitness Dir" && cp $2.httprobe $Tools/EyeWitness
+echo "\nStarting DirSearch for Basic Paths on Found Hosts"
 python3 $Tools/dirsearch/dirsearch.py -L $2.httprobe -e .* -w $Tools/paths --simple-report=$2.dirsearch
-echo "Checking Discovered Domains for Sub Takeovers"
+echo "\nChecking Discovered Domains for Sub Takeovers"
 subjack -w $2
--- MassDNSResolveTest.sh
+++ MassDNSResolveTest.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-
-/home/aidan/Tools/massdns/bin/massdns -r massdns/lists/resolvers.txt -t A -o S /home/aidan/BugBounty/Selectra/Subdomains.txt -w /home/aidan/BugBounty/Selectra/MassDNSResolve.txt
-
-print "[*] Finished Enumerating " 
-print "[*] Cleaning up with sed now"
-
-sed 's/A.*//' /home/aidan/BugBounty/Selectra/MassDNSResolve.txt | sed 's/CN.*//' | sed 's/\..$//' > /home/aidan/BugBounty/Selectra/MassDNSResolve.clean.txt
+echo "Arg1 = Subdomain, Arg2 =Output" 
+massdns -r resolvers.txt -t A -o J $1 | jq 'select(.resp_type=="A") | .query_name' | sort -u > $2
