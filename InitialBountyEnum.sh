9ae7fc9cc9a195602d249148605cdb9f1802fa03
m0chan <aidan@aidesigns.net>
2019-12-16 14:37:50 +0000

    NewScript

---
+++ InitialBountyEnum.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Syntax, Arg1 = Domain, Arg2 = Output File"
+echo "Enumerating " $1
+echo "Consider using amass intel -org to find ASN and bruteforce/rev dns lookup CIDR range etc"
+subfinder -d $1 -o $2.initialdomains
+amass enum -d $1 >> $2.initialdomains 
+python3 $Tools/CrtSH.py $1 >> $2.initialdomains
+cat $2.initialdomains | grep -v "Average" | grep -v "Querying" | awk '!seen[$0]++' $2.initialdomains > $2
+echo "Found The Following Amount of SubDomains (Will Check for SubTakeover & End" && wc -l $2
+
+echo "Starting Stage 2 - Service Discovery"
+cat $2 | httprobe -c 50 > $2.httprobe
+echo "Copying HTTP Servers to EyeWitness Dir" && cp $2.httprobe $Tools/EyeWitness
+echo "Starting DirSearch for Basic Paths on Found Hosts"
+python3 $Tools/dirsearch/dirsearch.py -L $2.httprobe -e .* -w $Tools/paths --simple-report=$2.dirsearch
+echo "Checking Discovered Domains for Sub Takeovers"
+subjack -w $2
